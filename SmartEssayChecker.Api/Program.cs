42d3c1859d5ccc5bbfdc26a7e850e19d540de6dd
Mirsaid04 <mirsaidd04@gmail.com>
2023-11-27 12:17:55 +0900

    INFRA: Intialize Api Project

--- SmartEssayChecker.Api/Program.cs
+++ SmartEssayChecker.Api/Program.cs
@@ -1,26 +1,20 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+//=================================
+// Copyright (c) Tarteeb LLC
+// Check your essays esily
+//=================================
+
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace SmartEssayChecker.Api
 {
     public class Program
     {
-        public static void Main(string[] args)
-        {
+        public static void Main(string[] args) =>
             CreateHostBuilder(args).Build().Run();
-        }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+                    webBuilder.UseStartup<Startup>());
     }
 }
