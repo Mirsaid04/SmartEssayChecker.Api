b11b579bd5faa31cfca8df556fcd0c2c3fa95028
Mirsaid04 <mirsaidd04@gmail.com>
2023-11-30 22:59:53 +0900

    FOUNDATIONS: Add Services

---
+++ SmartEssayChecker.Api/Services/Foundations/Essays/EssayService.Exceptions.cs
@@ -0,0 +1,42 @@
+﻿//=================================
+// Copyright (c) Tarteeb LLC
+// Check your essays esily
+//=================================
+
+using System.Threading.Tasks;
+using SmartEssayChecker.Api.Models.Essays;
+using Xeptions;
+using SmartEssayChecker.Api.Models.Essays.Exceptions;
+
+namespace SmartEssayChecker.Api.Services.Foundations.Essays
+{
+    public partial class EssayService
+    {
+        private delegate ValueTask<Essay> ReturningEssayFunction();
+        private async ValueTask<Essay> TryCatch(ReturningEssayFunction returningEssayFunction)
+        {
+            try
+            {
+                return await returningEssayFunction();
+            }
+
+            catch (NullEssayException nullEssayException)
+            {
+                throw CreateAndLogValidationException(nullEssayException);
+            }
+            catch (InvalidEssayException invalidEssayException)
+            {
+                throw CreateAndLogValidationException(invalidEssayException);
+            }
+        }
+
+        private EssayValidationException CreateAndLogValidationException(Xeption xeption)
+        {
+            var essayValidationException = new EssayValidationException(xeption);
+            this.loggingBroker.LogError(essayValidationException);
+
+            return essayValidationException;
+        }
+
+    }
+}
---
+++ SmartEssayChecker.Api/Services/Foundations/Essays/EssayService.Validation.cs
@@ -0,0 +1,63 @@
+﻿//=================================
+// Copyright (c) Tarteeb LLC
+// Check your essays esily
+//=================================
+
+using System;
+using SmartEssayChecker.Api.Models.Essays;
+using SmartEssayChecker.Api.Models.Essays.Exceptions;
+
+namespace SmartEssayChecker.Api.Services.Foundations.Essays
+{
+    public partial class EssayService
+    {
+        private void ValidationOnAdd(Essay essay)
+        {
+            ValidateEssayNotNull(essay);
+
+            Validate(
+                (Rule: IsInvalid(essay.EssayId), Parameter: nameof(essay.EssayId)),
+                (Rule: IsInvalid(essay.EssayId), Parameter: nameof(essay.Content)));
+        }
+
+        private void ValidateEssayId(Guid essayId)
+        {
+            Validate((Rule: IsInvalid(essayId), Parameter: nameof(essayId)));
+        }
+
+        private static dynamic IsInvalid(Guid essayId) => new
+        {
+            Condition = essayId == default,
+            Message = "Id is required"
+        };
+
+        private static dynamic IsInvalid(string text) => new
+        {
+            Condition = String.IsNullOrWhiteSpace(text) == default,
+            Message = "Text is required"
+        };
+
+        private static void ValidateEssayNotNull(Essay essay)
+        {
+            if (essay is null)
+            {
+                throw new NullEssayException();
+            }
+        }
+        private static void Validate(params (dynamic Rule, string Parameter)[] validations)
+        {
+            var invalidEssayException = new InvalidEssayException();
+
+            foreach ((dynamic rule, string parameter) in validations)
+            {
+                if (rule.Condition)
+                {
+                    invalidEssayException.UpsertDataList(
+                        key: parameter,
+                        value: rule.Message);
+                }
+            }
+            invalidEssayException.ThrowIfContainsErrors();
+        }
+    }
+}
