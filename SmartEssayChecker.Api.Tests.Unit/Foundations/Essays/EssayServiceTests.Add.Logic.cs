d61bdd398c25119ad9d2188f00261a73d0344eee
Mirsaid04 <mirsaidd04@gmail.com>
2023-12-04 13:34:11 +0900

    ShoudlThrowValidationExceptionOnAddIfEssayInvalidAndLogItAsync -> FAIL

---
+++ SmartEssayChecker.Api.Tests.Unit/Foundations/Essays/EssayServiceTests.Add.Logic.cs
@@ -0,0 +1,41 @@
+﻿//=================================
+// Copyright (c) Tarteeb LLC
+// Check your essays esily
+//=================================
+
+using FluentAssertions;
+using Force.DeepCloner;
+using Moq;
+using SmartEssayChecker.Api.Models.Essays;
+using Xunit;
+
+namespace SmartEssayChecker.Api.Tests.Unit.Foundations.Essays
+{
+    public partial class EssayServiceTests
+    {
+        [Fact]
+        public async Task ShouldAddEssayAsync()
+        {
+            // given
+            Essay randomEssay = CreateRandomEssay();
+            Essay inputEssay = randomEssay;
+            Essay persistedEssay = inputEssay;
+            Essay expectedEssay = persistedEssay.DeepClone();
+
+            this.storageBrokerMock.Setup(broker =>
+            broker.InsertEssayAsync(inputEssay))
+            .ReturnsAsync(expectedEssay);
+
+            // when
+            Essay actualEssay = await this.essayService.AddEssayAsync(inputEssay);
+
+            //then
+            actualEssay.Should().BeEquivalentTo(expectedEssay);
+
+            this.storageBrokerMock.Verify(broker =>
+            broker.InsertEssayAsync(inputEssay), Times.Once());
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
---
+++ SmartEssayChecker.Api.Tests.Unit/Foundations/Essays/EssayServiceTests.cs
@@ -0,0 +1,43 @@
+﻿//=================================
+// Copyright (c) Tarteeb LLC
+// Check your essays esily
+//=================================
+
+using System.Linq.Expressions;
+using Moq;
+using SmartEssayChecker.Api.Brokers.Loggings;
+using SmartEssayChecker.Api.Brokers.Storages;
+using SmartEssayChecker.Api.Models.Essays;
+using SmartEssayChecker.Api.Services.Foundations.Essays;
+using Tynamix.ObjectFiller;
+using Xeptions;
+
+namespace SmartEssayChecker.Api.Tests.Unit.Foundations.Essays
+{
+    public partial class EssayServiceTests
+    {
+        private readonly Mock<IStorageBroker> storageBrokerMock;
+        private readonly Mock<ILoggingBroker> loggingBrokerMock;
+        private readonly IEssayService essayService;
+
+        public EssayServiceTests()
+        {
+            this.storageBrokerMock = new Mock<IStorageBroker>();
+            this.loggingBrokerMock = new Mock<ILoggingBroker>();
+
+            this.essayService =
+                new EssayService(
+                storageBroker: this.storageBrokerMock.Object,
+                loggingBroker: this.loggingBrokerMock.Object);
+        }
+
+        private static Expression<Func<Xeption, bool>> SameExceptionAs(Xeption expectedException) =>
+            actualException => actualException.SameExceptionAs(expectedException);
+
+        private static Essay CreateRandomEssay() =>
+            CreateEssayFiller().Create();
+
+        private static Filler<Essay> CreateEssayFiller() =>
+            new Filler<Essay>();
+    }
+}
