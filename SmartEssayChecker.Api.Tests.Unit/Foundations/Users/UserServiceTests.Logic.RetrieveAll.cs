cb0de4a120884a4fdf360da1a005d11d0377710c
Mirsaid04 <mirsaidd04@gmail.com>
2023-12-08 08:18:05 +0900

    ShouldRetrieveAllUsers -> FAIL

---
+++ SmartEssayChecker.Api.Tests.Unit/Foundations/Users/UserServiceTests.Logic.RetrieveAll.cs
@@ -0,0 +1,42 @@
+﻿//=================================
+// Copyright (c) Tarteeb LLC
+// Check your essays esily
+//=================================
+
+using System.Linq;
+using FluentAssertions;
+using Force.DeepCloner;
+using Moq;
+using SmartEssayChecker.Api.Models.Users;
+using Xunit;
+
+namespace SmartEssayChecker.Api.Tests.Unit.Foundations.Users
+{
+    public partial class UserServiceTests
+    {
+        [Fact]
+        public void ShouldRetrieveAllUsers()
+        {
+            //given
+            IQueryable<User> randomUsers = CreateRandomUsers();
+            IQueryable<User> persistedUsers = randomUsers;
+            IQueryable<User> expectedUsers = persistedUsers.DeepClone();
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectAllUsers()).Returns(persistedUsers);
+
+            //when
+            IQueryable<User> actualUsers =
+                this.userService.RetrieveUsers();
+
+            //then
+            actualUsers.Should().BeEquivalentTo(expectedUsers);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectAllUsers(), Times.Once());
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
