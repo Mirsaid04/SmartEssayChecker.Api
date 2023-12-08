1741dff102ee3db0dab957ec1fc4cbd69041736d
Mirsaid04 <mirsaidd04@gmail.com>
2023-12-08 09:42:01 +0900

    ShouldRetrieveByIdAsync -> FAIL

---
+++ SmartEssayChecker.Api.Tests.Unit/Foundations/Essays/EssayServiceTests.Logic.RetrieveById.cs
@@ -0,0 +1,41 @@
+﻿//=================================
+// Copyright (c) Tarteeb LLC
+// Check your essays esily
+//=================================
+
+using FluentAssertions;
+using Force.DeepCloner;
+using Moq;
+using SmartEssayChecker.Api.Models.Essays;
+using Xunit;
+
+namespace SmartEssayChecker.Api.Tests.Unit.Foundations.Essays
+{
+    public partial class EssayServiceTests
+    {
+        [Fact]
+        public async Task ShouldRetrieveByIdAsync()
+        {
+            //given
+            Guid randomEssayId = Guid.NewGuid();
+            Guid inputEssayId = randomEssayId;
+            Essay randomEssay = CreateRandomEssay();
+            Essay persistedEssay = randomEssay;
+            Essay expectedEssay = persistedEssay.DeepClone();
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectEssayByIdAsync(inputEssayId))
+                .ReturnsAsync(persistedEssay);
+
+            //when
+            Essay actualEssay = await this.essayService
+                .RetrieveEssayByIdAsync(inputEssayId);
+
+            //then
+            actualEssay.Should().BeEquivalentTo(expectedEssay);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectUserByIdAsync(inputEssayId), Times.Once());
+        }
+    }
+}
