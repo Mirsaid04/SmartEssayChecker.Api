3ea0d7cfdc52763e831a94da99eaf8ca7a08116e
Mirsaid04 <mirsaidd04@gmail.com>
2023-12-11 18:40:08 +0900

    BROKER: Analyze Essay

---
+++ SmartEssayChecker.Api/Brokers/OpenAis/OpenAiBroker.Essay.Analysis.cs
@@ -0,0 +1,20 @@
+﻿//=================================
+// Copyright (c) Tarteeb LLC
+// Check your essays esily
+//=================================
+
+using System;
+using System.Threading.Tasks;
+using Azure.Core;
+using Standard.AI.OpenAI.Models.Services.Foundations.ChatCompletions;
+
+namespace SmartEssayChecker.Api.Brokers.OpenAis
+{
+    internal partial class OpenAiBroker
+    {
+        public async ValueTask<ChatCompletion> AnalyzeEssayAsync(ChatCompletion chatCompletion)
+        {
+            return await openAIClient.ChatCompletions.SendChatCompletionAsync(chatCompletion);
+        }
+    }
+}
