296f495853661de79b8cf05a3173bf66056c8227
Mirsaid04 <mirsaidd04@gmail.com>
2023-12-14 18:11:44 +0900

    SERVICES: Add

---
+++ SmartEssayChecker.Api/Services/Foundations/OpenAis/OpenAiService.Exception.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading.Tasks;
+using SmartEssayChecker.Api.Services.Foundations.OpenAis.Exceptions;
+
+namespace SmartEssayChecker.Api.Services.Foundations.OpenAis
+{
+    public partial class OpenAiService
+    {
+        private delegate ValueTask<string> ReturnOpenAiAsync();
+
+        private async ValueTask<string> TryCatch(ReturnOpenAiAsync returnOpenAiAsync)
+        {
+            try
+            {
+                return await returnOpenAiAsync();
+            }
+            catch (NullOpenAiException nullOpenAiException)
+            {
+                throw CreateAndLogValidationException(nullOpenAiException);
+            }
+            /* catch(Exception exception)
+             {
+                 var failedOpenAiServiceException =
+                     new FailedOpenAiServiceException(exception);
+             }*/
+        }
+
+        /*  private Exception CreateAndLogServiceException(Xeption exception)
+          {
+              var openAiServiceException =
+                  new OpenAiServiceException(exception);
+
+              this.loggingBroker.LogError(openAiServiceException);
+
+              return openAiServiceException;
+          }*/
+
+        private OpenAiValidationException CreateAndLogValidationException(Exception exceptionn)
+        {
+            var openAiValidationException =
+                new OpenAiValidationException(exceptionn);
+
+            this.loggingBroker.LogError(openAiValidationException);
+            return openAiValidationException;
+        }
+    }
+}
---
+++ SmartEssayChecker.Api/Services/Foundations/OpenAis/OpenAiService.Validation.cs
@@ -0,0 +1,24 @@
+﻿//=================================
+// Copyright (c) Tarteeb LLC
+// Check your essays esily
+//=================================
+
+using SmartEssayChecker.Api.Services.Foundations.OpenAis.Exceptions;
+
+namespace SmartEssayChecker.Api.Services.Foundations.OpenAis
+{
+    public partial class OpenAiService
+    {
+        private static void ValidateOpenAiOnAdd(string essay)
+        {
+            ValidateOpanAiIsNotNull(essay);
+        }
+        private static void ValidateOpanAiIsNotNull(string essay)
+        {
+            if (essay == null)
+            {
+                throw new NullOpenAiException();
+            }
+        }
+    }
+}
