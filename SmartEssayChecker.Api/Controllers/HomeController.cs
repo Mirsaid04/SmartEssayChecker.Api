f7a99a823520de2e65047d84cb2c38583ce4985d
Mirsaid04 <mirsaidd04@gmail.com>
2023-12-27 15:58:01 +0900

    CONTROLLERS: Update Features

--- SmartEssayChecker.Api/Controllers/HomeController.cs
+++ SmartEssayChecker.Api/Controllers/HomeController.cs
@@ -3,17 +3,10 @@
 // Check your essays easily
 //=================================
 
-using System;
-using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using RESTFulSense.Controllers;
-using SmartEssayChecker.Api.Brokers.OpenAis;
 using SmartEssayChecker.Api.Models.EssayAnalayses;
-using SmartEssayChecker.Api.Models.Essays;
-using SmartEssayChecker.Api.Models.Users.Exceptions;
-using SmartEssayChecker.Api.Services.Foundations.Essays;
-using SmartEssayChecker.Api.Services.Foundations.Users;
 using SmartEssayChecker.Api.Services.Orchestrations;
 
 namespace SmartEssayChecker.Api.Controllers
@@ -23,19 +16,10 @@ namespace SmartEssayChecker.Api.Controllers
     public class HomeController : RESTFulController
     {
         private readonly IEssayAnalysisOrchestrationService orchestrationService;
-        private readonly IEssayService essayService;
-        private readonly IOpenAiBroker broker;
-        private readonly IUserService userService;
 
-        public HomeController(IEssayAnalysisOrchestrationService orchestrationService,
-            IEssayService essayService,
-            IOpenAiBroker broker,
-            IUserService userService)
+        public HomeController(IEssayAnalysisOrchestrationService orchestrationService)
         {
             this.orchestrationService = orchestrationService;
-            this.essayService = essayService;
-            this.broker = broker;
-            this.userService = userService;
         }
 
         [HttpGet]
@@ -43,26 +27,15 @@ namespace SmartEssayChecker.Api.Controllers
             Ok("Hello Mario, the princess is in another castle.");
 
         [HttpPost]
-
-        public async Task<ActionResult<EssayAnalysis>> Post(string userName, string essay)
+        [Consumes("text/plain")]
+        public async Task<ActionResult<EssayAnalysis>> Post([FromBody] string essay)
         {
-            var user = this.userService.RetrieveUsers().FirstOrDefault(user => user.Name == userName);
+            var essayAnalyse = new EssayAnalysis();
+            essayAnalyse.Essay.Content = essay;
 
-            if (user == null)
-            {
-                throw new NotFoundUserByNameException(userName);
-            }
-            var essayAnalysis = await this.orchestrationService.AnalyzeEssay(new EssayAnalysis
-            {
-                Essay = new Essay
-                {
-                    Content = essay,
-                    EssayId = Guid.NewGuid(),
-                    UserId = user.Id
-                },
-            }, userName);
+            await orchestrationService.AnalyzeEssay(essayAnalyse);
 
-            return Ok(essayAnalysis);
+            return Ok(essayAnalyse);
         }
     }
 }
