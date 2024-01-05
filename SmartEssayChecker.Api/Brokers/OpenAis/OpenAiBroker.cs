8a26bd49e5e8838bbbcf1a19264866d641133404
Mirsaid04 <mirsaidd04@gmail.com>
2024-01-05 14:32:01 +0900

    CODE RUB: Refactor Code

--- SmartEssayChecker.Api/Brokers/OpenAis/OpenAiBroker.cs
+++ SmartEssayChecker.Api/Brokers/OpenAis/OpenAiBroker.cs
@@ -23,7 +23,7 @@ namespace SmartEssayChecker.Api.Brokers.OpenAis
 
         private OpenAIClient ConfigureOpenAIClient()
         {
-            string apiKey = "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";
+            string apiKey = "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";
 
             var openAIConfiguration = new OpenAIConfigurations
             {
