e70075fcda25b6c5c68d5c6659d79165014d5926
Mirsaid04 <mirsaidd04@gmail.com>
2024-01-05 14:30:55 +0900

    Revert "CODE RUB: Refactor Code"
    This reverts commit 533fa35a4c89294aec775a84847fa5f38439a2a1.

---
+++ SmartEssayChecker.Api/Brokers/OpenAis/OpenAiBroker.cs
@@ -0,0 +1,36 @@
+﻿//=================================
+// Copyright (c) Tarteeb LLC
+// Check your essays esily
+//=================================
+
+using Microsoft.Extensions.Configuration;
+using Standard.AI.OpenAI.Clients.OpenAIs;
+using Standard.AI.OpenAI.Models.Configurations;
+
+namespace SmartEssayChecker.Api.Brokers.OpenAis
+{
+    public partial class OpenAiBroker : IOpenAiBroker
+    {
+        private readonly OpenAIClient openAIClient;
+        private readonly IConfiguration configuration;
+
+        public OpenAiBroker(
+            IConfiguration configuration)
+        {
+            this.configuration = configuration;
+            this.openAIClient = ConfigureOpenAIClient();
+        }
+
+        private OpenAIClient ConfigureOpenAIClient()
+        {
+            string apiKey = "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";
+
+            var openAIConfiguration = new OpenAIConfigurations
+            {
+                ApiKey = apiKey,
+            };
+
+            return new OpenAIClient(openAIConfiguration);
+        }
+    }
+}
