9c0dd5d3412cac6f11c4221039a293c0f075aae3
Mirsaid04 <mirsaidd04@gmail.com>
2024-01-17 08:35:24 +0900

    Update

--- SmartEssayChecker.Api/Brokers/OpenAis/OpenAiBroker.cs
+++ SmartEssayChecker.Api/Brokers/OpenAis/OpenAiBroker.cs
@@ -22,7 +22,7 @@ namespace SmartEssayChecker.Api.Brokers.OpenAis
         }
         private OpenAIClient ConfigureOpenAIClient()
         {
-            string apiKey = "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";
+            string apiKey = "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";
 
             var openAIConfiguration = new OpenAIConfigurations
             {
